02c226b1c8083e38f15212261047ea327002248c
sh88688 <sh88688@gmail.com>
2020-01-31 18:20:38 +0530

    Applied today yesterday to dashboard

---
+++ dashboard_config.php
@@ -0,0 +1,5 @@
+<?php
+    define("_DEFAULT_RANGE","Today");
+    define("_DEFAULT_GRAPH","all");
+    $table_suffix = array("Today"=>"today","Yesterday"=>"yesterday","Last 7 Days"=>"7day","Last 30 Days"=>"30day");
+?>
--- dashboard_requirements.php
+++ dashboard_requirements.php
@@ -4,12 +4,16 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="assets/css/minified/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<!-- <link href="assets/css/minified/bootstrap.min.css" rel="stylesheet" type="text/css"> -->
+	<link rel="stylesheet" type="text/css" href="assets/css/minified/daterangepicker.min.css" />
 	<!-- <link rel="stylesheet" href="highcharts/css/bootstrap.min.css"> -->
-		<!-- <script src="assets/js/highcharts/ajax/jquery.min.js"></script> -->
-		<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
-    	<script src="assets/js/highcharts/highcharts.min.js"></script>
-    	<script src="assets/js/highcharts/modules/exporting.min.js"></script>
-    	<script src="assets/js/highcharts/modules/export-data.min.js"></script>
-    	<script src="assets/js/highcharts/modules/drilldown.min.js"></script>
+	<!-- <script src="assets/js/highcharts/ajax/jquery.min.js"></script> -->
+	<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+	<script type="text/javascript" src="javascript/ajax.min.js"></script>
+	 <script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
+	<script type="text/javascript" src="assets/js/daterangepicker.min.js"></script>
+	<script src="assets/js/highcharts/highcharts.min.js"></script>
+	<script src="assets/js/highcharts/modules/exporting.min.js"></script>
+	<script src="assets/js/highcharts/modules/export-data.min.js"></script>
+	<script src="assets/js/highcharts/modules/drilldown.min.js"></script>
 </head>
